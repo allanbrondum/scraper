f7869e04813b43e656b15bf652dbbc6d1bd15dbc
Allan Rasmussen <allanbrondum@gmail.com>
2022-01-27 15:55:07 +0100

    impl

--- src/bin/parsewikipagemain.rs
+++ src/bin/parsewikipagemain.rs
@@ -5,31 +5,18 @@ use std::collections::VecDeque;
 
 fn main() {
     // let page =
-    //     task::block_on(wiki::read_page("https://en.wikipedia.org/wiki/Gravity"));
+    //     task::block_on(wiki::read_page("http://en.wikipedia.org/wiki/Gravity"));
 
     let page =
-        wiki::read_page_sync("https://en.wikipedia.org/wiki/Gravity");
-
-    println!("{:?}", page);
+        wiki::read_page_sync("Gravity");
 
     match page {
         Ok(res) => {
-            println!("{}:", res.page);
-            for link in res.wiki_links {
+            println!("{}:", res.page_name);
+            for link in res.page_link_names {
                 println!("  {}", link);
             }
         },
         Err(err) => eprintln!("{}", err),
     };
-
-    let a : VecDeque<u8> = [1u8, 2u8].iter().copied().collect();
-    let mut b : VecDeque<u8> = VecDeque::with_capacity(2);
-    b.push_back(2u8);
-    b.push_back(1u8);
-    b.pop_front();
-    b.push_back(2u8);
-    println!("{:?}", a);
-    println!("{:?}", b);
-    println!("{}", a == b);
-
 }
--- src/wiki.rs
+++ src/wiki.rs
@@ -1,27 +1,31 @@
-use std::collections::VecDeque;
+use std::collections::{VecDeque, HashSet};
 
-#[derive(Debug, PartialEq, Eq, Clone, Hash)]
+#[derive(Debug, PartialEq, Eq, Clone)]
 pub struct WikiPage {
-    pub page: String,
-    pub wiki_links: Vec<String>,
+    pub page_name: String,
+    pub page_link_names: HashSet<String>,
 }
 
-pub async fn read_page(path: impl AsRef<str>) -> reqwest::Result<WikiPage> {
-    let response = reqwest::get(path.as_ref()).await?.error_for_status()?;
-    Ok(WikiPage{page: path.as_ref().to_string(), wiki_links: Vec::default()})
+pub async fn read_page(page_name: impl AsRef<str>) -> reqwest::Result<WikiPage> {
+    let response = reqwest::get(to_url(&page_name)).await?.error_for_status()?;
+    Ok(WikiPage{ page_name: page_name.as_ref().to_string(), page_link_names: HashSet::default()})
 }
 
-pub fn read_page_sync(path: impl AsRef<str>) -> reqwest::Result<WikiPage> {
-    let response = reqwest::blocking::get(path.as_ref())?.error_for_status()?;
+pub fn read_page_sync(page_name: impl AsRef<str>) -> reqwest::Result<WikiPage> {
+    let response = reqwest::blocking::get(to_url(&page_name))?.error_for_status()?;
     let wiki_links = read_links(response.bytes()?.iter().copied());
-    Ok(WikiPage{page: path.as_ref().to_string(), wiki_links})
+    Ok(WikiPage{ page_name: page_name.as_ref().to_string(), page_link_names: wiki_links })
 }
 
-fn read_links(mut bytes: impl Iterator<Item=u8>) -> Vec<String> {
+pub fn to_url(page_name: impl AsRef<str>) -> String {
+    "http://en.wikipedia.org/wiki/".to_string() + page_name.as_ref()
+}
+
+fn read_links(mut bytes: impl Iterator<Item=u8>) -> HashSet<String> {
     // this method is not exactly correct charset handling, but works for now
     let href_bytes : VecDeque<u8> = "<a href=\"/wiki/".bytes().collect();
     let mut buffer = VecDeque::with_capacity(href_bytes.len());
-    let mut links = Vec::new();
+    let mut link_page_names = HashSet::new();
     while let Some(byte) = bytes.next() {
         if buffer.len() == href_bytes.len() {
             buffer.pop_front();
@@ -38,9 +42,9 @@ fn read_links(mut bytes: impl Iterator<Item=u8>) -> Vec<String> {
             }
             let link = String::from_utf8(match_bytes).expect("invalid utf8");
             if !link.contains(":") && !link.contains("#") {
-                links.push(link);
+                link_page_names.insert(link);
             }
         }
     }
-    links
+    link_page_names
 }
